4592674d87ed328080c99c9dd657271f3fedd615
Cody Baker <cbaker9@nd.edu>
2020-11-09 17:36:30 -0500

    improved dateparse method

--- mease_lab_to_nwb/convert_syntalos/syntalosnwbconverter.py
+++ mease_lab_to_nwb/convert_syntalos/syntalosnwbconverter.py
@@ -1,17 +1,19 @@
 """Authors: Cody Baker and Ben Dichter."""
 from pathlib import Path
-from datetime import datetime
-from dateutil.parser import parse as dateparse
+from dateparser import parse as dateparse
 import toml
 
-from nwb_conversion_tools import NWBConverter, IntanRecordingInterface
+from nwb_conversion_tools import NWBConverter
+from syntaloseventinterface import SyntalosEventInterface
+from syntalosimageinterface import SyntalosImageInterface
 
 
 class SyntalosNWBConverter(NWBConverter):
     """Primary conversion class for Syntalos."""
 
     data_interface_classes = dict(
-        IntanRecording=IntanRecordingInterface
+        SyntalosEvent=SyntalosEventInterface,
+        SyntalosImage=SyntalosImageInterface
     )
 
     def get_metadata(self):
@@ -19,24 +21,18 @@ class SyntalosNWBConverter(NWBConverter):
         intan_filepath = Path(self.data_interface_objects['IntanRecording'].input_args['file_path'])
         session_id = intan_filepath.stem
         subject_id = toml.load(intan_filepath.parent.parent / "attributes.toml")['subject_id']
-        session_start = dateparse(
-            "".join([name for name in session_id.split('_')if name.isdigit()]),
-            yearfirst=True,
-            dayfirst=True
-        )
+        session_start = dateparse(date_string=session_id[-13:], date_formats=["%y%m%d_%H%M%S"])
         metadata = super().get_metadata()
         metadata.update(
             NWBFile=dict(
                 identifier=session_id,
                 session_start_time=session_start.astimezone(),
-                file_create_date=datetime.now().astimezone(),
                 session_id=session_id,
                 institution="EMBL - Heidelberg",
                 lab="Mease"
             ),
             Subject=dict(
                 subject_id=subject_id
-            ),
-            # IntanAccelerometer=dict()
+            )
         )
         return metadata
