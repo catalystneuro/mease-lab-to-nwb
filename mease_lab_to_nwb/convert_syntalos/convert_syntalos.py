6e658e11f2b6e2533b49497277c131461bdbcab7
Cody Baker <cbaker9@nd.edu>
2020-11-09 14:48:43 -0500

    added ephys component to syntalos

--- mease_lab_to_nwb/convert_syntalos/convert_syntalos.py
+++ mease_lab_to_nwb/convert_syntalos/convert_syntalos.py
@@ -1,57 +1,34 @@
-"""Authors: Cody Baker and Ben Dichter."""
-# TODO: add pathlib
-import os
-
-from joblib import Parallel, delayed
+"""Authors: Alessio Buccino, Cody Baker, Szonja Weigl, and Ben Dichter."""
+from pathlib import Path
 
 from mease_lab_to_nwb import SyntalosNWBConverter
 
-n_jobs = 1  # number of parallel streams to run
 
-# TODO: Need to update base_path
-base_path = "D:/Heidelberg_data/CED_example_data"
+base_path = Path("D:/Syntalos/Latest Syntalos Recording _20200730")
+# originally, this intan file had a -002 attached to the end before the .rhd, but spikeextractors complained
+intan_file_path = base_path / "intan-signals" / "169d_data_200714_121455.rhd"
+event_file_path = base_path / "events" / "table.csv"
+video_folder_path = base_path / "videos" / "TIS Camera"
+nwbfile_path = base_path / "Syntalos_stub.nwb"
 
-# Manual list of selected sessions that cause problems with the general functionality
-exlude_sessions = []
+if base_path.is_dir():
+    input_args = dict(
+        IntanRecording=dict(
+            file_path=intan_file_path
+        ),
+        # IntanAccelerometer=dict(
+        #     file_path=intan_file_path
+        # )
+    )
 
-virmen_session_strings = []
-spikeglx_session_strings = []
-nwbfile_paths = []
-for j, (virmen_session, spikeglx_session) in enumerate(zip(virmen_session_strings, spikeglx_session_strings)):
-    virmen_session_strings[j] = os.path.join(base_path, virmen_session_strings[j])
-    spikeglx_session_strings[j] = os.path.join(base_path, spikeglx_session_strings[j])
-    nwbfile_paths.append(os.path.join(base_path, virmen_session) + "_local_stub.nwb")  # name defaults to virmen session
+    converter = SyntalosNWBConverter(**input_args)
+    metadata = converter.get_metadata()
 
+    # Session specific metadata
+    metadata['NWBFile'].update(session_description="")
 
-def run_syntalos_conv(virmen_session, spikeglx_session, nwbfile_path):
-    """Conversion function to be run in parallel."""
-    if os.path.exists(base_path):
-        print(f"Processsing {virmen_session}...")
-        if not os.path.isfile(nwbfile_path):
-            input_args = dict(
-                SpikeGLXRecording=dict(file_path=spikeglx_session+".imec0.ap.bin"),
-                TowersPosition=dict(folder_path=virmen_session)
-            )
-
-            converter = SyntalosNWBConverter(**input_args)
-            metadata = converter.get_metadata()
-
-            # Session specific metadata
-            metadata['NWBFile'].update(session_description="")
-
-            # metadata[yuta_converter.get_recording_type()]['Ecephys']['Device'][0].update({'name': 'implant'})
-
-            # for electrode_group_metadata in \
-            #         metadata[yuta_converter.get_recording_type()]['Ecephys']['ElectrodeGroup']:
-            #     electrode_group_metadata.update({'location': 'unknown'})
-            #     electrode_group_metadata.update({'device_name': 'implant'})
-
-            converter.run_conversion(nwbfile_path=nwbfile_path, metadata_dict=metadata, stub_test=True)
-    else:
-        print(f"The folder ({base_path}) does not exist!")
-
-
-Parallel(n_jobs=n_jobs)(delayed(run_syntalos_conv)(virmen_session, spikeglx_session, nwbfile_path)
-                        for virmen_session, spikeglx_session, nwbfile_path
-                        in zip(virmen_session_strings, spikeglx_session_strings, nwbfile_paths)
-                        if os.path.split(virmen_session)[1] not in exlude_sessions)
+    converter.run_conversion(
+        nwbfile_path=str(nwbfile_path.absolute()),
+        metadata_dict=metadata,
+        stub_test=True
+    )
