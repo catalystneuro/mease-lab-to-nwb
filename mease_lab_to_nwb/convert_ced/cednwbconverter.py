ffd4ab6b136decfe7eaa7b24da92dd380a2dd66b
luiztauffer <luiz_tauffer@hotmail.com>
2020-11-17 15:11:46 +0100

    remove unused imports

--- mease_lab_to_nwb/convert_ced/cednwbconverter.py
+++ mease_lab_to_nwb/convert_ced/cednwbconverter.py
@@ -1,12 +1,5 @@
 """Authors: Cody Baker and Ben Dichter."""
-import os
-from datetime import datetime, timedelta
-
-from dateutil.parser import parse as dateparse
-from isodate import duration_isoformat
 from nwb_conversion_tools import NWBConverter, CEDRecordingInterface
-
-from ..utils import convert_mat_file_to_dict
 
 
 class CEDNWBConverter(NWBConverter):
