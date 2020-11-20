6770c212fd2873b02dd84afd6a7b8e5c9426d587
Alessio Buccino <alejoe9187@gmail.com>
2020-11-20 10:41:59 +0100

    Increase efficiency of SyntalosRecordingExtractor

--- mease_lab_to_nwb/convert_syntalos/syntalosrecordingextractor.py
+++ mease_lab_to_nwb/convert_syntalos/syntalosrecordingextractor.py
@@ -25,6 +25,7 @@ class SyntalosRecordingExtractor(se.IntanRecordingExtractor):
             raise AttributeError(f"{file_or_folder_path.name} is not a Syntalos folder or an rhd file")
 
         super().__init__(rhd_file)
+        self._times = None
 
         if len(tsync_files) == 0:
             self._sync = False
@@ -41,60 +42,31 @@ class SyntalosRecordingExtractor(se.IntanRecordingExtractor):
                 except:
                     self._sync = False
             if self._sync:
-                data_len = self.get_num_frames()
-                sample_rate = self.get_sampling_frequency()
-                sync_map = tsync.times
-                data_pos_idx = 0
-                sync_idx = 0
-                offset_usec = sync_map[0, 0] - sync_map[0, 1]
-
-                tvec, offset_usec, sync_idx = make_synced_tsvec(data_len,
-                                                                sample_rate,
-                                                                sync_map,
-                                                                data_pos_idx,
-                                                                sync_idx,
-                                                                offset_usec)
-
-                self._times = np.squeeze(tvec) / 1000
-
-        self._recording = se.IntanRecordingExtractor
+                self.sync_map = tsync.times
+                self.sync_times()
 
         self._kwargs = {'folder_path': str(file_or_folder_path.absolute())}
 
     def frame_to_time(self, frame):
         return self._times[frame]
 
-    # TODO use bisect
     def time_to_frame(self, time):
-        tdiffs = self._times - time
-        return np.argmin(tdiffs).astype('int64')
+        return np.searchsorted(self._times, time).astype('int64')
 
+    def sync_times(self):
+        tv_usec = np.arange(self.get_num_frames(), dtype=np.float64) / self.get_sampling_frequency() * 1E6
+        tv_adj_usec = np.arange(self.get_num_frames(), dtype=np.float64) / self.get_sampling_frequency() * 1E6
 
-### Helper functions from edlio.dataio.rhd.read_rhc
-def first_ge_index(vec, c):
-    ''' Get the index of the first greater-equal number to `c`
-    in ascending-sorted vector `vec` '''
-    for i in range(len(vec)):
-        if vec[i] >= c:
-            return i
-    return np.nan
+        init_offset_usec = offset_usec = self.sync_map[0][0] - self.sync_map[0][1]
+        idx = np.where(tv_usec <= self.sync_map[0][0])[0]
+        tv_adj_usec[idx] -= init_offset_usec
 
+        for s, sync in enumerate(self.sync_map):
+            if s < len(self.sync_map) - 1:
+                idx = np.where((tv_usec > sync[0]) & (tv_usec <= self.sync_map[s + 1][0]))[0]
+                offset_usec = self.sync_map[s + 1][0] - self.sync_map[s + 1][1]
+            else:
+                idx = np.where(tv_usec > sync[0])[0]
+            tv_adj_usec[idx] -= offset_usec
 
-def make_synced_tsvec(data_len, sample_rate, sync_map, index_offset, init_sync_idx, init_offset_usec):
-    ''' Create time vector, synchronizing all timepoints. '''
-    offset_usec = init_offset_usec
-    sync_idx = init_sync_idx
-
-    tv_adj = np.zeros((data_len, 1), dtype=np.float64)
-    for i in range(data_len):
-        ts_msec = ((i + index_offset) / sample_rate) * 1000
-        ts_usec = ts_msec * 1000
-
-        ns_idx = first_ge_index(sync_map[:, 0][sync_idx:], ts_usec) + sync_idx
-        if ns_idx > sync_idx:
-            sync_idx = ns_idx
-            offset_usec = sync_map[sync_idx][0] - sync_map[sync_idx][1]
-
-        tv_adj[i] = ts_msec - (offset_usec / 1000)
-
-    return tv_adj, offset_usec, sync_idx
+        self._times = tv_adj_usec / 1E6
