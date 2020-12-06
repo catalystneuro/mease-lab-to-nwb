d34470c9a6bb906a3b70f2c65642d86bcc33c414
Cody Baker <cbaker9@nd.edu>
2020-12-06 12:33:54 -0500

    extended accelerometer method and conversion itself to multirecordings

--- mease_lab_to_nwb/convert_syntalos/convert_syntalos.py
+++ mease_lab_to_nwb/convert_syntalos/convert_syntalos.py
@@ -1,28 +1,27 @@
 """Authors: Alessio Buccino, Cody Baker, Szonja Weigl, and Ben Dichter."""
 from pathlib import Path
 
-from .syntalosnwbconverter import SyntalosNWBConverter
+from syntalosnwbconverter import SyntalosNWBConverter
 
 base_path = Path("D:/Syntalos/Latest Syntalos Recording _20200730")
-intan_file_path = base_path / "intan-signals" / "169d_data_200714_121455.rhd"
+intan_folder_path = base_path / "intan-signals"
 event_file_path = base_path / "events" / "table.csv"
 video_folder_path = base_path / "videos" / "TIS Camera"
 nwbfile_path = base_path / "Syntalos_stub.nwb"
 
-if base_path.is_dir():
-    input_args = dict(
-        SyntalosEvent=dict(file_path=str(event_file_path.absolute())),
-        SyntalosImage=dict(folder_path=str(video_folder_path.absolute())),
-        IntanRecording=dict(file_path=str(intan_file_path.absolute()), dtype="uint16")
-    )
-    conversion_options = dict(
-        IntanRecording=dict(stub_test=True)
-    )
+source_data = dict(
+    SyntalosEvent=dict(file_path=str(event_file_path.absolute())),
+    SyntalosImage=dict(folder_path=str(video_folder_path.absolute())),
+    SyntalosRecording=dict(folder_path=str(intan_folder_path.absolute()))
+)
+conversion_options = dict(
+    SyntalosRecording=dict(stub_test=True)
+)
 
-    converter = SyntalosNWBConverter(**input_args)
-    metadata = converter.get_metadata()
-    converter.run_conversion(
-        nwbfile_path=str(nwbfile_path.absolute()),
-        metadata=metadata,
-        conversion_options=conversion_options
-    )
+converter = SyntalosNWBConverter(source_data)
+metadata = converter.get_metadata()
+converter.run_conversion(
+    nwbfile_path=str(nwbfile_path.absolute()),
+    metadata=metadata,
+    conversion_options=conversion_options
+)
--- mease_lab_to_nwb/convert_syntalos/syntalosnwbconverter.py
+++ mease_lab_to_nwb/convert_syntalos/syntalosnwbconverter.py
@@ -5,9 +5,9 @@ import toml
 from dateparser import parse as dateparse
 from nwb_conversion_tools import NWBConverter, IntanRecordingInterface
 
-from .syntaloseventinterface import SyntalosEventInterface
-from .syntalosimageinterface import SyntalosImageInterface
-from .syntalosrecordinginterface import SyntalosRecordingInterface
+from syntaloseventinterface import SyntalosEventInterface
+from syntalosimageinterface import SyntalosImageInterface
+from syntalosrecordinginterface import SyntalosRecordingInterface
 
 class SyntalosNWBConverter(NWBConverter):
     """Primary conversion class for Syntalos."""
@@ -20,9 +20,9 @@ class SyntalosNWBConverter(NWBConverter):
 
     def get_metadata(self):
         """Auto-populate as much metadata as possible."""
-        intan_file_path = Path(self.data_interface_objects['SyntalosRecording'].source_data['file_path'])
-        session_id = intan_file_path.stem
-        subject_id = toml.load(intan_file_path.parent.parent / "attributes.toml")['subject_id']
+        intan_folder_path = Path(self.data_interface_objects['SyntalosRecording'].source_data['folder_path'])
+        session_id = [x for x in intan_folder_path.iterdir() if x.suffix == ".rhd"][0].stem
+        subject_id = toml.load(intan_folder_path.parent / "attributes.toml")['subject_id']
 
         session_start = dateparse(date_string=session_id[-13:], date_formats=["%y%m%d_%H%M%S"])
         metadata = super().get_metadata()
