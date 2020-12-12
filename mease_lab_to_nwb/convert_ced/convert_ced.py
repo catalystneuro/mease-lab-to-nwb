7f6fcaaeed4b8232c865508539fbed81fa1286eb
Cody Baker <cbaker9@nd.edu>
2020-12-12 15:50:01 -0500

    moved channel retrieval to CED converter class

--- mease_lab_to_nwb/convert_ced/convert_ced.py
+++ mease_lab_to_nwb/convert_ced/convert_ced.py
@@ -1,25 +1,16 @@
 """Authors: Cody Baker and Ben Dichter."""
 from pathlib import Path
-from spikeextractors import CEDRecordingExtractor
 
-from cednwbconverter import CEDNWBConverter
+from mease_lab_to_nwb import CEDNWBConverter
 
 
 base_path = Path("D:/CED_example_data/Other example")
 ced_file_path = base_path / "m365_pt1_590-1190secs-001.smrx"
 nwbfile_path = base_path / "CED_stub.nwb"
 
-channel_info = CEDRecordingExtractor.get_all_channels_info(ced_file_path)
-
-rhd_channels = []
-for ch, info in channel_info.items():
-    if "Rhd" in info["title"]:
-        rhd_channels.append(ch)
-
 source_data = dict(
     CEDRecording=dict(
-        file_path=str(ced_file_path.absolute()),
-        smrx_channel_ids=rhd_channels
+        file_path=str(ced_file_path.absolute())
     )
 )
 conversion_options = dict(
@@ -29,7 +20,7 @@ conversion_options = dict(
 converter = CEDNWBConverter(source_data)
 metadata = converter.get_metadata()
 converter.run_conversion(
-    nwbfile_path=str(nwbfile_path.absolute()),
+    nwbfile_path=str(nwbfile_path),
     metadata=metadata,
     conversion_options=conversion_options
 )
